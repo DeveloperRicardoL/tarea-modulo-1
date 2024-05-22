308b95b61d8c789223b25c19286a15c463c5c3b3
DeveloperRicardoL <ricardoleonardo097@gmail.com>
2024-05-22 17:17:30 -0300

    first-commit

---
+++ main.js
@@ -0,0 +1,16 @@
+import "./style.css";
+
+const botonEnviar = document.querySelector("#boton_enviar");
+const resp = document.querySelector("#resp");
+
+function traerNombre() {
+  let nom = document.getElementById("nombre").value.toUpperCase();
+
+  console.log(`Hola ${nom}, ¿como estas?`);
+  return nom;
+}
+
+botonEnviar.addEventListener("click", () => {
+  console.log(traerNombre());
+  resp.innerText = `Hola ${traerNombre()} 👋​`;
+});
